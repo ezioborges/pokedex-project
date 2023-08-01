dac93fcf8e6549fb707699c8614096f1e4a56615
Ruben <ruben.mafra@gmail.com>
2023-08-01 16:55:35 -0300

    iniciando tela de login

--- pokedex/src/Pages/Login.jsx
+++ pokedex/src/Pages/Login.jsx
@@ -1,5 +1,37 @@
-export default function Login () {
- return (
-    <h1>Login</h1>
- );
+import React from "react"
+
+export default function Login (props) {
+  return (
+    <div className="Auth-form-container d-flex justify-content-center mt-4 ">
+      <form className="Auth-form"  style={ { width: '55%', border: '1px solid black' } } >
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  )
 }
