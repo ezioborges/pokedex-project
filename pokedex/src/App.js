ab8ed1d9fcf50e8421c8b90d6550d3ce17a9ed75
Ezio Borges <eziopborges@gmail.com>
2023-08-01 14:18:14 -0300

    ajuste rotas para Login e Home

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
-import Main from "./Pages/Main";
+import Login from "./Pages/Login";
 import NotFound from "./Pages/NotFound";
 import PokemonDetails from "./Pages/PokemonDetails";
 import PokemonLocation from "./Pages/PokemonLocation";
+import Home from "./Pages/Home";
 
 const Routes = () => {
    return(
     <Switch>
-      <Route exact path="/" component = { Main } />
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/home" component={ Home } />
       <Route exact path="/pokemon-details/:id" component={ PokemonDetails } />
       <Route exact path="/pokemon-maps/:id" component={ PokemonLocation } />
       <Route path="*" component={ NotFound } />
